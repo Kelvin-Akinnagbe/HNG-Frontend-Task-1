6b854be89365d38013c840a2a9c4c6c683cfe7fa
Kelvin-Akinnagbe <115592732+Kelvin-Akinnagbe@users.noreply.github.com>
2023-09-10 19:48:36 +0100

    Update index.js
    Added descriptor texts.

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-   // I created this function to update the current day of the week
+   // I created this function to update the current day of the week. A descriptor text is concatenated to dayOfWeek. 
    function updateDayOfWeek() {
     const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const currentDate = new Date();
     const dayOfWeek = daysOfWeek[currentDate.getUTCDay()];
-    document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = dayOfWeek;
+    document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = "Today is: " + dayOfWeek;
 }
 
-// This function updates the current UTC time
+// This function updates the current UTC time. A descriptor text is concatenated to utcTime. 
 function updateUTCTime() {
     const currentDate = new Date();
     const utcTime = currentDate.getTime();
-    document.querySelector('[data-testid="currentUTCTime"]').textContent = utcTime;
+    document.querySelector('[data-testid="currentUTCTime"]').textContent ="The UTC time in milliseconds is: " + utcTime;
 }
 
 // This updates the day of the week and UTC time every second
